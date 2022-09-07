b3d688f406e489f5b3b41b0701d1e3f83cabbef1
BemMusicianApp <bemusicianapp@gmail.com>
2022-09-07 14:20:04 +0200

    push log out

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,19 @@ import { useContext } from "react";
 import { AuthContext } from "../contexts/authContext";
 import "../css/header.css";
 import "../musicfont/styles.css";
+import { createElement } from "react";
 
 const Header = () => {
     const { auth } = useContext(AuthContext);
+
+    function popUpLogOut(){
+        const popUp = document.createElement("div");
+        popUp.style.width = 100;
+        popUp.style.height = 100;
+        popUp.textContent = "êtes vous sur de vouloir vous déconnecter ?"
+        alert(popUp);
+    }
+
     return (
             <div className="navigation">
                     <nav className="contentLogo">
@@ -20,11 +30,14 @@ const Header = () => {
                         {auth.role === 1 && (
                             <Link to="/profil" className="navitem" id="profil"><i class="icon-user"></i></Link>               
                         )}
+                        {auth.role === 1 && (
+                            <Link to="/creation" className="navitem" id="create"><i class="icon-plus"></i></Link>               
+                        )}
                         {auth.role === 2 && (
                             <Link to="/admin" className="navitem" id="choixinstrument">admin</Link>
                         )}
                             {auth.role === 1 && (
-                            <Link to="/logout" className="navitem" id="logout"><i class="icon-sign-out"></i></Link>               
+                            <Link to="/logout" className="navitem" id="logout"><i class="icon-sign-out" onclick={popUpLogOut}></i></Link>               
                         )}
                   </nav>       
             </div>
--- src/Screens/logoutscreen.jsx
+++ src/Screens/logoutscreen.jsx
@@ -5,18 +5,35 @@ import { AuthContext } from "../contexts/authContext";
 const LogoutScreen = () => {
 
 const navigate = useNavigate();
-const {auth, setAuth} = useContext(AuthContext);
-const cookieStr = 'token=vide ; max-age=0';
-document.cookie = cookieStr;
 
-useEffect(() =>{
-    setTimeout(()=>{
-        setAuth ({role:0});
-        navigate("/login");
-    },1000);
-}) 
+
+    // useEffect(() =>{
+    //     setTimeout(()=>{
+    //         setAuth ({role:0});
+    //         navigate("/login");
+    //     },1000);
+    // }) 
+    const {auth, setAuth} = useContext(AuthContext);
+    function deconnexion(){
+        const cookieStr = 'token=vide ; max-age=0';
+        document.cookie = cookieStr;
+        setTimeout(()=>{
+            setAuth ({role:0});
+            navigate("/login");
+        },0);        
+    }
+    function retourAcceuil(){
+        navigate("/")
+    }
+
     return (
-        <h1>Deconnexion ... </h1>
+        <div id="logOutContent">
+           <div id="logOutText"> C'est votre dernier mot ? </div>
+        <div>
+        <button id="resterConnecter" onClick={retourAcceuil}> Oula non je reste ! </button>
+        <button id="deconnexion" onClick={deconnexion}> Oui jean pierre ! </button>
+        </div>
+        </div>
     );
 }
 
